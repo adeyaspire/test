868410cbd0626ffe5bb4c73df78b7c2e007fa51e
unknown <adey@aspire-infotech.net>
2024-07-01 03:27:25 +0530

    flask backend app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+@app.route("/")
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
+
+
 @app.route("/api/data")
 def get_data():
     data = {"message": "Hello from Flask!"}
@@ -10,4 +16,4 @@ def get_data():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
